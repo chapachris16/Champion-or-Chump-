e5e44dd2e83147b1ca422e707ed32242cf87c47d
christopher chapa <chapachristopher16@gmail.com>
2022-07-26 21:52:53 -0500

    backend work

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,14 +1,11 @@
-// routes/api/users.js
-
 const express = require('express');
 const router = express.Router();
 const postsCtrl = require('../../controllers/api/posts');
 
-// POST /api/users
-
-router.post('/', postCtrl.create)
-router.get('/', postCtrl.find)
-router.get('/:postId', postCtrl.findOne)
+router.post('/', postsCtrl.create)
+router.get('/', postsCtrl.find)
+router.get('/:postId', postsCtrl.findOne)
 router.delete('/:postId', postsCtrl.remove)
 router.put('/:postId/edit', postsCtrl.edit)
+
 module.exports = router;
--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+// const axios = require('axios').default
 export const CreatePostForm = () => {
+  const [form, setForm] = useState({
+    title: "",
+    content: "",
+    likes: 0,
+    created: new Date(),
+  });
+
+  function createPost() {
+    axios.post("/api/posts", form).then((result) => {
+      console.log(result);
+    });
+  }
+
+  const handleChange = (evt) => {
+    setForm({ ...form, [evt.target.name]: evt.target.value, error: "" });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    createPost();
+    setForm({ title: "", content: "", likes: 0, created: new Date() });
+  };
   return (
-    <div>CreatePostForm</div>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit}>
+        Title
+        <input
+          type="text"
+          value={form.title}
+          name="title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={form.content}
+          name="content"
+          onChange={handleChange}
+        />
+        <input
+          type="hidden"
+          value={form.likes}
+          name="likes"
+          onChange={handleChange}
+        />
+        <input
+          type="hidden"
+          value={form.created}
+          name="created"
+          onChange={handleChange}
+        />
+        <button type="submit">SUBMIT</button>
+      </form>
+    </>
+  );
+};
