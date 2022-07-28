a57fca5d3bb652b5903f765b63cbf00eb5249131
christopher chapa <chapachristopher16@gmail.com>
2022-07-27 20:16:19 -0500

    conditional deleting for comments only made by the user

--- src/pages/CommentDetail/CommentDetail.jsx
+++ src/pages/CommentDetail/CommentDetail.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import { useParams, Navigate, useNavigate } from 'react-router-dom'
+import { getUser } from '../../utilities/users-service'
 export const CommentDetail = () => {
     let navigate = useNavigate()
     let {postId, commentId} = useParams()
     const [comment, setComment] = useState()
+    const [user, setUser] = useState(getUser())
     useEffect(() => {
         axios.get(`/api/posts/${postId}/comments/${commentId}`).then((foundComment) => {
             setComment(foundComment.data);
@@ -18,13 +20,18 @@ export const CommentDetail = () => {
             console.log(deletedComment);
           });
         navigate(`/posts/${postId}`)
+    
     }
     if (!comment) return null
   return (
     <>
         <p>{comment.post.comments[comment.index].author}</p>
         <p>{comment.post.comments[comment.index].content}</p>
+        {user.ign === comment.post.comments[comment.index].author?
         <button onClick={deleteComment}>Delete This Comment?</button>
+        :
+        <></>
+        }
     </>
   )
 }
